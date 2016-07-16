a9d618fc2f0df5b7051eb3b76c3c8fe42b6ec4b9
vinaypuppal <me@vinaypuppal.com>
2016-07-17 00:43:01 +0530

    added some dummy data

--- client/components/image_list.js
+++ client/components/image_list.js
@@ -8,11 +8,22 @@ import React from 'react';
 //Import custom components
 import ImageDetail from './image_detail';
 
+//Dummy data
+const IMAGES = [
+  {title:'ABC',link:'http://dummyimage.com/200x200/cc2bcc/fff.png&text=ABC'},
+  {title:'CDE',link:'http://dummyimage.com/200x200/cc2bcc/fff.png&text=CDE'},
+  {title:'FGH',link:'http://dummyimage.com/200x200/cc2bcc/fff.png&text=FGH'}
+]
+
 //Create component
 const ImageList = ()=>{
+  const renderedImages = IMAGES.map((image)=>{
+    return <ImageDetail />
+  });
+
   return (
     <ul>
-      <ImageDetail />
+      {renderedImages}
     </ul>
   );
 }
